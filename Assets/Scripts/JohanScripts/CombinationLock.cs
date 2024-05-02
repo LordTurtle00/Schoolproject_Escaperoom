380e35fafcedb6bd6c82b8e397681c8ef0dbb3e6
LordTurtle00 <jabbildmappskonto@gmail.com>
2024-05-02 14:30:45 +0200

    Update CombinationLock.cs

--- Assets/Scripts/JohanScripts/CombinationLock.cs
+++ Assets/Scripts/JohanScripts/CombinationLock.cs
@@ -44,35 +44,34 @@ public class CombinationLock : MonoBehaviour
             Debug.Log("Nice");
         }
     }
-    //IEnumerator Check()
-    //{
-        //yield return new WaitForSeconds(1);
-       
-        
-        
-    //}
-    private void Update()
+    IEnumerator Check()
+    {
+        yield return new WaitForSeconds(1);
+        for (int i = 0; i<AmountOfDials.Length; i++)
+        {
+            Dials d = AmountOfDials[i].GetComponent<Dials>();
+            d.CheckRotation();
+            
+        }
+        RightCobination();
+
+    }
+private void Update()
     {
         // Use Ienumerator to make so that in the update function the update happens every second insted of every frame
         // Move the CheckRotation function from PickupAndDrop to this update function so that the rotation of the dials are checked every few seconds
         // Same with the RightCobination funktion move it to this update function to check every few seconds that the combination of the lock is correct
         if (Answer == false)
         {
-             for (int i = 0; i < AmountOfDials.Length; i++)
-             {
-                Dials d = AmountOfDials[i].GetComponent<Dials>();
-                 d.CheckRotation();
             
-             }
-             RightCobination();
-            //StartCoroutine(Check());
+            StartCoroutine(Check());
             Debug.Log("working?");
         }
 
 
         if (Answer == true)
         {
-            //StopCoroutine(Check());
+            StopCoroutine(Check());
             Debug.Log("Plz Stop");
         }
         
